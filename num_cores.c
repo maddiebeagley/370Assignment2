db80db05a77a06469e8a23e02d4d951069a135af
maddiebeagley <beagleymaddie@gmail.com>
2018-09-11 13:36:46 +1200

    fixed bug in retrieve number of processors method

--- num_cores.c
+++ num_cores.c
@@ -2,15 +2,14 @@
 #include <sys/sysinfo.h>
 
 
-int main() {
+int get_num_cores(){
 	int num_cores = get_nprocs();
 	printf("This machine has %d core(s).\n", num_cores);
 	return num_cores;
 }
 
-int def get_num_cores(){
-	int num_cores = get_nprocs();
-	printf("This machine has %d core(s).\n", num_cores);
-	return num_cores;
+int main() {
+	get_num_cores();
 }
+
 
